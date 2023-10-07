2bb07fc6d3365e21c3a59a14a768c1e9d14db3bb
ThoPham02 <2021050819@student.humg.edu.vn>
2023-10-07 10:43:44 +0700

    giai phuong trinh bac 2

--- MVCProject/Controllers/HomeController.cs
+++ MVCProject/Controllers/HomeController.cs
@@ -6,26 +6,27 @@ namespace MVCProject.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
+    public IActionResult Index() {
+        ViewBag.result = "Vui lòng nhập tham số trước!!";
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    [HttpPost]
+    public IActionResult Index(int a, int b, int c) {
+        string resultStr = "";
+        int delta = b * b - 4 * a * c;
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        if (delta < 0) {
+            resultStr = "Phương trình vừa nhập vô nghiệm";
+        } else if (delta == 0) {
+            double x = -b * 1.0 /2/a;
+            resultStr = "Phương trình có nghiệm kép: " + x;
+        } else {
+            double x1 = ( -b - Math.Sqrt(delta)) / (2 * a);  
+            double x2 = ( -b + Math.Sqrt(delta)) / (2 * a); 
+            resultStr = "Phương trình có 2 nghiệm phân biệt: " + x1 + " và " + x2; 
+        }
+        ViewBag.result = resultStr;
+        return View();
     }
 }
